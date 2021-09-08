869e5e736ff5cd351c4858de4fbbfc43936f3e82
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-09-08 15:32:23 -0500

    :recycle: Clean Up

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,53 +1,31 @@
-import React from 'react'
-import Button from 'react-bootstrap/Button'
-import Card from 'react-bootstrap/Card'
 
-/** Quotation card Powered by the Quotable API https://github.com/lukePeavey/quotable */
+import React, { useState, useEffect } from 'react'
+
+import { Card } from '../containers/main'
+
 const Main = () => {
-  const [data, setData] = React.useState(null)
+  const [data, setData] = useState(null)
 
   async function updateQuote() {
-    try {
-      const response = await fetch('https://api.quotable.io/random')
-      const { statusCode, statusMessage, ...data } = await response.json()
-      if (!response.ok) throw new Error(`${statusCode} ${statusMessage}`)
-      setData(data)
-    } catch (error) {
-      // If the API request failed, log the error to console and update state
-      // so that the error will be reflected in the UI.
-      console.error(error)
-      setData({ content: 'Opps... Something went wrong' })
-    }
+    const response = await fetch('https://api.quotable.io/random')
+    setData(await response.json())
   }
 
-  // Run `updateQuote` once when component mounts
-  React.useEffect(() => {
+  useEffect(() => {
     updateQuote()
   }, [])
 
-  // Do not render until the first quote is loaded
   if (!data) return null
 
   return (
-    <div className="App">
-      <Card style={{ width: '90%', maxWidth: '40rem' }}>
-        <Card.Body>
-          <blockquote className="blockquote mb-0">
-            <p>{data.content}</p>
-            {data.author && (
-              <footer className="blockquote-footer">
-                <cite title="Source Title">{data.author}</cite>
-              </footer>
-            )}
-          </blockquote>
-        </Card.Body>
-        <Card.Footer>
-          <Button variant="primary" onClick={updateQuote}>
-            New Quote
-          </Button>
-        </Card.Footer>
-      </Card>
-    </div>
+    <>
+      <Card
+        content={data.content}
+        author={data.author}
+        authorSlug={data.authorSlug}
+        update={updateQuote}
+      />
+    </>
   )
 }
 
